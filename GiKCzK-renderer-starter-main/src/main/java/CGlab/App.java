1098e4634657ca4348fc421a2632f2d962337ddf
Karol66 <karolbal66@gmail.com>
2023-03-26 20:56:53 +0200

    commit

---
+++ GiKCzK-renderer-starter-main/src/main/java/CGlab/App.java
@@ -0,0 +1,40 @@
+package CGlab;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App {
+
+    String version = "0.02";
+
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Niepoprawne argumenty! Użyj: java App [ścieżka] [szerokość] [wysokość]");
+            return;
+        }
+
+        String filePath = args[0];
+        Integer width = Integer.parseInt(args[1]);
+        Integer height = Integer.parseInt(args[2]);
+
+        System.out.println(filePath);
+        System.out.println(width);
+        System.out.println(height);
+
+        Renderer mainRenderer = new Renderer(filePath);
+        mainRenderer.clear();
+//        mainRenderer.drawPoint(width, height);
+        mainRenderer.drawLineNaive(10, 1,30,90);
+
+        try {
+            mainRenderer.save();
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+}
